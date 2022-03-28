6ab4f30cee6471607b9df0eebd7f1055ed78a34f
KangGeonyoung <kky6335@gmail.com>
2022-03-28 10:27:22 +0900

    Baekjoon : No.6(3)

--- Baekjoon/함수/1065.py
+++ Baekjoon/함수/1065.py
@@ -1,9 +1,8 @@
 N = int(input())
-digit = []
-result = 0
 
 def findHansu(N) :
-    total = 0
+    digit = []
+    result = 0
 
     for i in range(1, N + 1) :
         digit.clear()
@@ -15,14 +14,26 @@ def findHansu(N) :
             digit.append(num % 10)
             num = num // 10
 
+        # 한 자릿수 또는 두 자릿수 일때
         if len(digit) == 1 or len(digit) == 2:
             result += 1
             continue
 
+        # 세 자릿수부터는 여기에서 한수인지 검사
         calculate = digit[0] - digit[1]
+        isHansu = False
 
         for j in range(len(digit) - 1) :
             if digit[j] - digit[j+1] != calculate :
+                isHansu = False
                 break
             else :
                 calculate = digit[j] - digit[j+1]
+                isHansu = True
+        
+        if isHansu == True :
+            result += 1
+    
+    return result
+
+print(findHansu(N))
