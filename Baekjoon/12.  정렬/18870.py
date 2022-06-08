cb25acfa1b11e1cb2c0038cc0bd372ad035d9e0c
KangGeonyoung <kky6335@gmail.com>
2022-06-08 14:57:52 +0900

    Baekjoon : No.12(4)

--- Baekjoon/12.  정렬/18870.py
+++ Baekjoon/12.  정렬/18870.py
@@ -1,10 +1,21 @@
 N = int(input())
 num = list(map(int, input().split()))
+Min = min(num)
+
+for i in range(len(num)) :
+    num[i] += abs(Min)
 
 newNum = set(num)
 newNum = list(newNum)
 
 newNum.sort()
 
+# dictionary를 통해 결과 값을 저장
+dic = {}
+for i in range(len(newNum)) :
+    dic[i] = newNum[i]
+
+reverse_dic = dict(map(reversed, dic.items()))
+
 for i in range(N) :
-    print(newNum.index(num[i]) ,end=' ')
+    print(reverse_dic[num[i]], end=' ')
