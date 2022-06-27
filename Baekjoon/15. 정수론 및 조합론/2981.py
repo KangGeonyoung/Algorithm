bb95b9bebfc872013992334894adf3a74c642a11
KangGeonyoung <kky6335@gmail.com>
2022-06-27 15:12:42 +0900

    Baekjoon : No.15(3)

--- Baekjoon/15. 정수론 및 조합론/2981.py
+++ Baekjoon/15. 정수론 및 조합론/2981.py
@@ -1,30 +1,56 @@
+import math
 import sys
+
+def GCD(n1, n2) :
+    if n2 == 0 :
+        return n1
+    
+    return GCD(n2, n1%n2)
 
 N = int(sys.stdin.readline())
 num = list()
-result = list()
+newNum = list()
+gcd = 0
+factor = list()
 
 for i in range(N) :
     num.append(int(sys.stdin.readline()))
 
-Min = min(num)
+num = set(num)
+num = list(num)
 
-# M 범위
-for i in range(2, Min+1) :
-    state = 1
-    # 한 줄씩 검사
-    for j in range(len(num)) :
-        if j+1 == len(num) :
-            break
+for i in range(len(num)) :
+    if len(num) == 1 :
+        newNum.append(num[0])
+        break
 
-        if num[j] % i != num[j+1] % i :
-            state = 0
-            break
-    
-    if state == 1 :
-        result.append(i)
+    if i+1 == len(num) :
+        break
+    newNum.append(abs(num[i+1] - num[i]))
 
-for i in range(len(result)) :
-    print(result[i], end=' ')
+for i in range(len(newNum)) :
+    # 값이 하나일 때
+    if len(newNum) == 1 :
+        gcd = newNum[0]
+        break
 
-print(result)
+    # indexOutOf 예방
+    if i+1 == len(newNum) :
+        break
+
+    # 두 개씩 최대공약수를 찾아서 gcd에 업데이트
+    gcd = GCD(newNum[i], gcd)
+
+
+for i in range(2, int(math.sqrt(gcd))+1) :
+    if gcd % i == 0 :
+        factor.append(i)
+        factor.append(int(gcd/i))
+factor.append(gcd)
+
+factor = set(factor)
+factor = list(factor)
+factor.sort()
+
+for i in range(len(factor)) :
+    print(factor[i], end=' ')
