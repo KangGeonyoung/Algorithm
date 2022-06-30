54fa5d572250b14e81bc3c5427e380ee37830a05
KangGeonyoung <kky6335@gmail.com>
2022-07-01 00:23:20 +0900

    Baekjoon : No.15(5)

--- Baekjoon/15. 정수론 및 조합론/3036.py
+++ Baekjoon/15. 정수론 및 조합론/3036.py
@@ -1,10 +1,17 @@
+# 기약 분수로 만드는 방법
+# : GCD(최대공약수)를 구해서 나눠주면 약분이 되어 기약 분수가 된다.
 import sys
 
+def GCD(n1, n2) :
+    if n2 == 0 :
+        return n1
+    
+    return GCD(n2, n1 % n2)
 
 N = int(sys.stdin.readline())
-
 ling = list(map(int, sys.stdin.readline().split()))
 
-print(ling)
+for i in range(1, N) :
+    gcd = GCD(ling[0], ling[i])
 
-
+    print(f"{int(ling[0] / gcd)}/{int(ling[i] / gcd)}")
