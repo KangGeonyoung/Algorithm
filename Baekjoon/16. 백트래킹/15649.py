2b8a03b51601a2207993013fbfe23361cf01c429
KangGeonyoung <kky6335@gmail.com>
2022-07-06 17:50:34 +0900

    Baekjoon : No.16(1)

---
+++ Baekjoon/16. 백트래킹/15649.py
@@ -0,0 +1,21 @@
+import sys
+
+def sequence(origin, present, M) :
+    # present의 크기와 M이 같다면
+    if len(present) == M :
+        for i in range(len(present)) :
+            print(present[i], end=" ")
+        print()
+    else :
+        for i in range(len(origin)) :
+            if origin[i] not in present :
+                present.append(origin[i])
+                sequence(origin, present, M)
+                present.remove(origin[i])
+
+
+N, M = map(int, sys.stdin.readline().split())
+origin = [i+1 for i in range(N)]
+present = list()
+
+sequence(origin, present, M)
---
+++ Baekjoon/16. 백트래킹/15650.py
@@ -0,0 +1,31 @@
+import sys
+
+def sequence(origin, present, M) :
+    check = ""
+
+    # present의 크기와 M이 같다면
+    if len(present) == M :
+        for i in range(len(present)) :
+            if i+1 < len(present) :
+                if present[i] > present[i+1] :
+                    break
+            check += f"{present[i]} "
+        if len(check) == M * 2 :
+            result.append(check)
+    else :
+        for i in range(len(origin)) :
+            if origin[i] not in present :
+                present.append(origin[i])
+                sequence(origin, present, M)
+                present.remove(origin[i])
+
+
+N, M = map(int, sys.stdin.readline().split())
+origin = [i+1 for i in range(N)]
+present = list()
+result = list()
+
+sequence(origin, present, M)
+
+for i in range(len(result)) :
+    print(result[i])
