7efce0fb7bcddc6033a90f42e0f20618d965d990
KangGeonyoung <kky6335@gmail.com>
2022-07-10 23:07:23 +0900

    Baekjoon : No.16(4)

--- Baekjoon/16. 백트래킹/9663.py
+++ Baekjoon/16. 백트래킹/9663.py
@@ -1,8 +1,11 @@
+import copy
 import sys
 
-def N_queen(x, y) :
-    global queenCnt, result, chess
+def N_queen(x, y, chess, preChess, queenCnt) :
+    global result, N
 
+    preChess = copy.deepcopy(chess)
+    
     print(f"n_queen {x}, {y}")
     # 퀸을 체스판에 놓았기 때문에 queenCnt +1 해주기
     queenCnt += 1
@@ -21,29 +24,34 @@ def N_queen(x, y) :
             if (i+j) == (x+y) or abs(i-j) == abs(x-y) :
                 chess[i][j] = 0
 
-            # if chess[i][j] == (x+y) or chess[i][j] == abs(x-y) :
-            #     chess[i][j] = 0
+    
 
     # 퀸을 체스판에 다 놓았다면 경우의 수(result) 증가 시키기
     if queenCnt == N :
         result += 1
-        queenCnt = 0
-        return 0
+        return chess
+
+    print(chess)
 
     # 다음 퀸 자리 찾는 과정
     for i in range(N) :
         for j in range(N) :
             if chess[i][j] == 1 :
-                N_queen(i, j)
+                
+                chess = N_queen(i, j, chess, preChess, queenCnt)
+    
+    if queenCnt != N :
+        return preChess
 
-
+    
 N = int(sys.stdin.readline())
 result = 0
-queenCnt = 0
-chess = [[1 for col in range(N)] for row in range(N)]
 
 for i in range(N) :
     for j in range(N) :
-            N_queen(i, j)
+        chess = [[1 for col in range(N)] for row in range(N)]
+        preChess = copy.deepcopy(chess)
+        queenCnt = 0
+        chess = N_queen(i, j, chess, preChess, queenCnt)
 
 print(result)
