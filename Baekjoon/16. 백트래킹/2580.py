760707f3794ced204ac3eecd6931e3ee0488f8d7
KangGeonyoung <kky6335@gmail.com>
2022-07-25 17:12:00 +0900

    Baekjoon : No.16(7)

--- Baekjoon/16. 백트래킹/2580.py
+++ Baekjoon/16. 백트래킹/2580.py
@@ -1,67 +1,48 @@
 import sys
 
-def sudoku(i, j) :
-    row = list()
-    col = list()
-    square = list()
+def checkRow(y, a) :
+    for i in range(9) :
+        if a == board[y][i] :
+            return False
+    return True
 
-    # row에서 없는 숫자 검사
-    number = [1,2,3,4,5,6,7,8,9]
-    for m in range(9) :
-        if board[i][m] in number :
-            number[number.index(board[i][m])] = 0
-    for m in range(9) :
-        if number[m] != 0 :
-            row.append(number[m])
+def checkCol(x, a) :
+    for i in range(9) :
+        if a == board[i][x] :
+            return False
+    return True
 
-    # col에서 없는 숫자 검사
-    number = [1,2,3,4,5,6,7,8,9]
-    for m in range(9) :
-        if board[m][j] in number :
-            number[number.index(board[m][j])] = 0
-    for m in range(9) :
-        if number[m] != 0 :
-            col.append(number[m])
+def checkSquare(y, x, a) :
+    ny = y // 3 * 3
+    nx = x // 3 * 3
+    for i in range(3) :
+        for j in range(3) :
+            if a == board[ny + i][nx + j] :
+                return False
+    return True
 
-    # 3*3 사각형에서 없는 숫자 검사
-    number = [1,2,3,4,5,6,7,8,9]
-    start_row = i // 3 * 3
-    start_col = j // 3 * 3
+def dfs(idx) :
+    if idx == len(blank) :
+        for i in range(9) :
+            print(*board[i])
+        exit(0)
+    
+    for k in range(1, 10) :
+        y = blank[idx][0]
+        x = blank[idx][1]
+        if checkRow(y, k) and checkCol(x, k) and checkSquare(y, x, k) :
+            board[y][x] = k
+            dfs(idx+1)
+            board[y][x] = 0
 
-    for m in range(3) :
-        for n in range(3) :
-            if board[start_row + m][start_col + n] in number :
-                number[number.index(board[start_row + m][start_col + n])] = 0
-    for m in range(9) :
-        if number[m] != 0 :
-            square.append(number[m])
 
-    # 3개의 검사 결과 중에 중복되는 숫자 추출
-    intersection = list(set(row) & set(col) & set(square))
-    print(f"intersection : {intersection}")
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(9)]
+blank = []
 
-    # 찾은 숫자 보드에 대입
-    if len(intersection) > 0 :
-        board[i][j] = intersection[0]
-
-    # 종료
-    return
-
-# 스도쿠 보드 세팅
-board = [[0 for col in range(9)] for row in range(9)]
-
-# input 입력 받는 코드
-for i in range(9) :
-    board[i] = list(map(int, sys.stdin.readline().split()))
-
-# 0을 발견하면 sudoku 함수 실행
 for i in range(9) :
     for j in range(9) :
         if board[i][j] == 0 :
-            sudoku(i, j)
+            blank.append((i, j))
 
-# 스도쿠 결과 출력
-for i in range(9) :
-    for j in range(9) :
-        print(board[i][j], end=" ")
-    print()
+
+dfs(0)
